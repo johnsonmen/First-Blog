844b6414b865f2e9687650a77a0443e92b901165
Johnson Men <johnsonmen@Johnsons-M1.home>
2023-09-27 21:23:26 -0700

    finalized

--- clean.py
+++ clean.py
@@ -4,12 +4,13 @@ import numpy as np
 
 def range_hyphen_remover(range_value):
     '''
-    This function removes the hyphen from the range of values with a hyphen and returns the average of the bounds
-    Input - range value - a string with a hyphen
-    Output - the average of the two numbers in the range
+    removes the hyphen from a range value and returns the average of the bounds
+    input: range value - a string with a hyphen
+    output: value - the average of the upperbounds and lowerbounds in the range
     '''
     if '–' in range_value:
         range_value = range_value.split('–')
-        return (float(range_value[0]) + float(range_value[1])) / 2
+        value = float(range_value[0]) + float(range_value[1]) / 2
     else:
-        return float(range_value)
+        value float(range_value)
+    return value
--- test_hyphen_remover.py
+++ test_hyphen_remover.py
@@ -6,23 +6,48 @@ from clean import range_hyphen_remover
 epsilon = sys.float_info.epsilon
 
 def test_hyphen_remover_1_2():
+    '''
+    tests function range_hyphen_remover with int bounds and float output
+    input: string '1–2'
+    output: float 1.5
+    '''
     assert(range_hyphen_remover('1–2') - 1.5 < epsilon)
 
 def test_hyphen_remover_1_3():
+    '''
+    tests function range_hyphen_remover with int bounds and output
+    input: string '1–3'
+    output: int 2
+    '''
     assert(range_hyphen_remover('1–3') == 2)
 
-def test_hyphen_remover_10_200():
-    assert(range_hyphen_remover('10–200') == 105)
-
 def test_hyphen_remover_float1():
+    '''
+    tests function range_hyphen_remover with float bounds and int output
+    '''
     assert(range_hyphen_remover('1.5–2.5') == 2)
 
 def test_hyphen_remover_float2():
+    '''
+    tests function range_hyphen_remover with float bounds and output
+    input: string '34.0–39.2'
+    output: float 36.6
+    '''
     assert(range_hyphen_remover('34.0–39.2') - 36.6 < epsilon)
 
 def test_hyphen_remover_float3():
+    '''
+    tests function range_hyphen_remover with float bounds and output
+    input: string '51.2–54.3'
+    output: float 52.75
+    '''
     assert(range_hyphen_remover('51.2–54.3') - 52.75 < epsilon)
 
 def test_hyphen_remover_str():
+    '''
+    tests function range_hyphen_remover with float input and output
+    input: string '40.5'
+    output: float 40.5
+    '''
     assert(range_hyphen_remover('40.5') - 40.5 < epsilon)
 
